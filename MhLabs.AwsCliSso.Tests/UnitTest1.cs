39c2da8112025efcbf76d77ee4edd017017be0d6
Viktor Stenqvist <viktor.stenqvist@mathem.se>
2020-03-05 15:41:13 +0100

    Add test project

---
+++ MhLabs.AwsCliSso.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace MhLabs.AwsCliSso.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Yay!
+            Assert.True(true);
+        }
+    }
+}
